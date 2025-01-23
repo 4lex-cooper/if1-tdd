18b426e9a27f31bd934dcd8a4a771d36f40bf69a
4lex-cooper <alex_cooper@live.co.uk>
2025-01-23 18:03:17 +0000

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(input_value):
+    if input_value % 15 == 0:
+        return "fizzbuzz"
+    elif input_value % 3 == 0:
+        return "fizz"
+    elif input_value % 5 == 0:
+        return "buzz"
+    else:
+        return input_value
+
+
+for i in range(1, 101):
+    print(fizzbuzz(i))
